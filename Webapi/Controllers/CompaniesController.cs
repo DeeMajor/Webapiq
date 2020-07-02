86e13ae30f085d9baf317808d65a2d93b75910d3
Andile <andilebshange@gmail.com>
2020-07-02 15:41:10 +0200

    Add project files.

---
+++ Webapi/Controllers/CompaniesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Webapi.Models;
+
+namespace Webapi.Controllers
+{
+    public class CompaniesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Companies
+        public IQueryable<Company> GetCompanies()
+        {
+            return db.Companies;
+        }
+
+        // GET: api/Companies/5
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult GetCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company);
+        }
+
+        // PUT: api/Companies/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCompany(int id, Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        //upload image fo folder
+        [Route("user/PostUserImage")]
+        [AllowAnonymous]
+        public HttpResponseMessage PostUserImage()
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            try
+            {
+
+                var httpRequest = HttpContext.Current.Request;
+
+                foreach (string file in httpRequest.Files)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+                    var postedFile = httpRequest.Files[file];
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+
+                        int MaxContentLength = 1024 * 1024 * 1; //Size = 1 MB  
+
+                        IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".gif", ".png" };
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = ext.ToLower();
+                        if (!AllowedFileExtensions.Contains(extension))
+                        {
+
+                            var message = string.Format("Please Upload image of type .jpg,.gif,.png.");
+
+                            dict.Add("error", message);
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+                        else if (postedFile.ContentLength > MaxContentLength)
+                        {
+
+                            var message = string.Format("Please Upload a file upto 1 mb.");
+
+                            dict.Add("error", message);
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+                        else
+                        {
+
+
+
+                            var filePath = HttpContext.Current.Server.MapPath("~/Userimage/" + postedFile.FileName + extension);
+
+                            postedFile.SaveAs(filePath);
+
+                        }
+                    }
+
+                    var message1 = string.Format("Image Updated Successfully.");
+                    return Request.CreateErrorResponse(HttpStatusCode.Created, message1); ;
+                }
+                var res = string.Format("Please Upload a image.");
+                dict.Add("error", res);
+                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+            }
+            catch (Exception ex)
+            {
+                var res = string.Format("some Message");
+                dict.Add("error", res);
+                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+            }
+        }
+        // POST: api/Companies
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult PostCompany(Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Companies.Add(company);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = company.Id }, company);
+        }
+
+        // DELETE: api/Companies/5
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult DeleteCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            db.Companies.Remove(company);
+            db.SaveChanges();
+
+            return Ok(company);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return db.Companies.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Webapi/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webapi.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string location { get; set; }
+        [AllowHtml]
+        public string Contents { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
